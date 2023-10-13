abb3fe3b6a948c573d18d794e3dc5a5c18e4250b
Nilesh Bhaskar <73060066+NileshBhaskar@users.noreply.github.com>
2023-10-13 07:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+touristSpots.sort( (a,b) => a-b);
+
+let u = document.getElementsByTagName("ul")[0];
+
+for(let i=0;i<touristSpots.length;i++){
+	u.InnerHTML += (`<li>${touristSpots[i]}</li>`);
+}
