b398098a312d6c1545c190f7483cb4a18096ff03
Nilesh Bhaskar <73060066+NileshBhaskar@users.noreply.github.com>
2023-10-13 13:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let u = document.getElementsByTagName("ul")[0];
 
 for(let i=0;i<touristSpots.length;i++){
   const liElement = document.createElement('li');
-  liElement.textContent = touristSpots[i];
+  // liElement.textContent = touristSpots[i];
+	liElement.innerHTML = touristSpots[i];
   u.appendChild(liElement);
 }
